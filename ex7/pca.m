4a2a7a49a340c5e63c5a3e0ce799b8cf65938ce7
Mohamed Adnan Azmie <76649244+Goose-max@users.noreply.github.com>
2021-12-18 23:37:29 +0400

    ex7
    Exercise 7 - MATLAB Code

---
+++ ex7/pca.m
@@ -0,0 +1,30 @@
+function [U, S] = pca(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+%
+
+% Useful values
+[m, n] = size(X);
+
+% You need to return the following variables correctly.
+U = zeros(n); %#ok<*PREALL> 
+S = zeros(n);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should first compute the covariance matrix. Then, you
+%               should use the "svd" function to compute the eigenvectors
+%               and eigenvalues of the covariance matrix. 
+%
+% Note: When computing the covariance matrix, remember to divide by m (the
+%       number of examples).
+%
+% Covarience Matrix
+XT_X = X'*X; 
+Sigma = (1/m) * XT_X;
+% Eigenvectors & Eigenvalues - convarience matrix
+[U,S,~] = svd(Sigma);
+
+% =========================================================================
+
+end
